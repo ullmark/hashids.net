d768c83b67d994bffe727ae44df8c0a425547354
Markus Ullmark <ullmark@gmail.com>
2015-08-14 10:12:40 +0200

    closes #8

---
+++ Hashids.net.test/Hashids_bugs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HashidsNet.test
+{
+    public class Hashids_bugs
+    {
+        [Fact]
+        public void issue_8_should_not_throw_out_of_range_exception()
+        {
+            var hashids = new Hashids("janottaa", 6);
+            var numbers = hashids.Decode("NgAzADEANAA=");
+        }
+    }
+}
