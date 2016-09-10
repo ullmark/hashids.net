0ab88f68f5bf6f8321b59c6ee488a00e63b116e7
Markus Ullmark <ullmark@gmail.com>
2016-09-10 14:40:02 +0200

    added a crude perf test to validate PR

---
+++ test/Hashids.net.test/Hashids_perf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using System.Diagnostics;
+
+namespace HashidsNet.test
+{
+    public class Hashids_perf
+    {
+        static Random random = new Random();
+
+        [Fact]
+        void Encode_single()
+        {
+            var hashids = new Hashids();
+            var stopWatch = Stopwatch.StartNew();
+            for (var i = 1; i < 10001; i++)
+            {
+                hashids.Encode(i);
+            }
+            stopWatch.Stop();
+            Trace.WriteLine($"10 000 encodes: {stopWatch.ElapsedMilliseconds}");
+        }
+    }
+}
