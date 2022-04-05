b2c78773d05fcf8c9edc643f87db31e98554f2f0
Mani Gandham <manigandham@gmail.com>
2022-04-05 01:12:00 -0700

    Update tests (#67)
    * improve comment and usage for buffer size
    - no need for single use const, move commentary to code to make usage more clear
    
    * fix bug in issue 15 test to actually use declared vars
    
    * tests cleanup, fix naming, explicit params
    
    * use parallel.for in multithreading test
    - more reliable tests using partitioner in parallel method instead of list of tasks
    - wont swallow exceptions inside tasks
    - decrease total test running time

--- test/Hashids.net.test/IssueSpecificTests.cs
+++ test/Hashids.net.test/IssueSpecificTests.cs
@@ -8,7 +8,7 @@ namespace HashidsNet.test
     public class IssueSpecificTests
     {
         [Fact]
-        void issue_8_should_not_throw_out_of_range_exception()
+        void Issue_8_should_not_throw_out_of_range_exception()
         {
             var hashids = new Hashids("janottaa", 6);
             var numbers = hashids.Decode("NgAzADEANAA=");
@@ -18,7 +18,7 @@ namespace HashidsNet.test
         // seems to happen when you are encoding A LOT of longs at the same time.
         // see if it is possible to make this a faster test (or remove it since it is unlikely that it will reapper).
         [Fact]
-        void issue_12_should_not_throw_out_of_range_exception()
+        void Issue_12_should_not_throw_out_of_range_exception()
         {
             var hash = new Hashids("zXZVFf2N38uV");
             var longs = new List<long>();
@@ -38,23 +38,23 @@ namespace HashidsNet.test
         }
 
         [Fact]
-        void issue_15_it_should_return_emtpy_array_when_decoding_characters_missing_in_alphabet()
+        void Issue_15_it_should_return_empty_array_when_decoding_characters_missing_in_alphabet()
         {
             var hashids = new Hashids(salt: "Salty stuff", alphabet: "qwerty1234!¤%&/()=", seps: "1234");
             var numbers = hashids.Decode("abcd");
             numbers.Length.Should().Be(0);
 
             var hashids2 = new Hashids();
-            hashids.Decode("13-37").Length.Should().Be(0);
-            hashids.DecodeLong("32323kldffd!").Length.Should().Be(0);
+            hashids2.Decode("13-37").Length.Should().Be(0);
+            hashids2.DecodeLong("32323kldffd!").Length.Should().Be(0);
 
             var hashids3 = new Hashids(alphabet: "1234567890;:_!#¤%&/()=", seps: "!#¤%&/()=");
-            hashids.Decode("asdfb").Length.Should().Be(0);
-            hashids.DecodeLong("asdfgfdgdfgkj").Length.Should().Be(0);
+            hashids3.Decode("asdfb").Length.Should().Be(0);
+            hashids3.DecodeLong("asdfgfdgdfgkj").Length.Should().Be(0);
         }
 
         [Fact]
-        void issue_64_it_should_be_possible_to_encode_and_decode_long_max_value()
+        void Issue_64_long_max_value_with_min_alphabet_length()
         {
             var hashids = new Hashids("salt", alphabet: "0123456789ABCDEF");
             var hash = hashids.EncodeLong(long.MaxValue);
