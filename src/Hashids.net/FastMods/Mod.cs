88639a9344ab6a4a3740fb5caf243ac8ff5314b5
Vlad Kobelev <vko@stylepit.com>
2022-04-15 17:22:32 +0300

    Implement FastMod class

---
+++ src/Hashids.net/FastMods/Mod.cs
@@ -0,0 +1,21 @@
+﻿namespace HashidsNet
+{
+    public struct Mod
+    {
+        private uint _factor;
+        private uint _divisor;
+
+        public Mod(uint factor, uint divisor)
+        {
+            _factor = factor;
+            _divisor = divisor;
+        }
+
+        public static int operator %(int value, Mod mod)
+        {
+            uint lowBits = ((uint)value * mod._factor) & 0xFFFFu;
+
+            return (int)(lowBits * mod._divisor) >> 16;
+        }
+    }
+}
---
+++ src/Hashids.net/FastMods/Mods.cs
@@ -0,0 +1,22 @@
+﻿namespace HashidsNet
+{
+    public struct Mods
+    {
+        private Mod[] _mods;
+
+        public Mods(Mod[] mods)
+        {
+            _mods = mods;
+        }
+
+        public Mod Get(int divisor)
+        {
+            return _mods[divisor];
+        }
+
+        public int Mod(int value, int divisor)
+        {
+            return value % _mods[divisor];
+        }
+    }
+}
