d3dcfb84e6c8c3644e8224e16260fb4463047beb
07prajwal2000 <manuaradhya07@gmail.com>
2022-04-25 20:15:05 +0530

    Added Support for Microsoft.Extensions.DependencyInjection Container

---
+++ src/Hashids.net.Microsoft.Extensions.DependencyInjection/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using HashidsNet;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hashids.net.Microsoft.Extensions.DependencyInjection;
+
+public static class ExtensionMethods
+{
+    public const string DEFAULT_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    public const string DEFAULT_SEPS = "cfhistuCFHISTU";
+    
+    public static void AddHashIdsNet(this IServiceCollection services,
+        string salt = "",
+        int minHashLength = 0,
+        string alphabet = DEFAULT_ALPHABET,
+        string seps = DEFAULT_SEPS)
+    {
+        services.AddScoped<IHashids, HashidsNet.Hashids>(_ => new HashidsNet.Hashids(salt, minHashLength, alphabet, seps));
+    }
+}
